1bbbc89d080ea60a5ae86e6147842e03b25c76e8
Amey <ameyhande@gmail.com>
2019-03-23 23:31:32 -0400

    Added robot movement

--- mini_slam/src/sensor_data_creator.cpp
+++ mini_slam/src/sensor_data_creator.cpp
@@ -4,6 +4,8 @@
 #include "nav_msgs/OccupancyGrid.h"
 #include <sensor_msgs/PointCloud2.h>
 #include <sensor_msgs/point_cloud2_iterator.h>
+#include <boost/thread.hpp>
+#include <bits/stdc++.h>
 
 class Get_map_sensor_data
 {
@@ -13,18 +15,21 @@ class Get_map_sensor_data
 		nav_msgs::OccupancyGrid World_Map;
 		bool map_generated = false;
 		bool cloud_generated = false;
+		bool cloud_published = false;
 		sensor_msgs::PointCloud2 pointCloud;
+		int way_points[7][3] = {{7,2,0},{7,2,90},{7,7,90},{7,7,180},{2,7,180},{2,7,270},{2,2,270}};
 		int robot_pose[3] = {0,2,0}; // x-coordinate, y-coordinate and rotation about +ve z-axis (CCW is +ve)
 		ros::Publisher cloud_pub_ = nh_.advertise<sensor_msgs::PointCloud2>("/cloud", 10);
+		bool robot_ready = true; // Added to control goal sending loop
+		int robot_move_rate = 1;
 		
 
 		
 	public:
 		Get_map_sensor_data(void)
-		{			
+		{
 			Get_map_sensor_data::create_Map();
 			Get_map_sensor_data::generate_pointCloud();
-			Get_map_sensor_data::publish_pointCloud();
 		}
 
 		~Get_map_sensor_data(void)
@@ -53,14 +58,13 @@ class Get_map_sensor_data
 					}
 				}
 			}
-			//World_Map.data[]
 			map_generated = true;
 		}
 		
 		void generate_pointCloud()
 		{
 			ros::Rate rate(5);			
-			while(!map_generated)
+			while(!map_generated && ros::ok())
 			{
 				rate.sleep();
 			}
@@ -87,17 +91,18 @@ class Get_map_sensor_data
 			for (int i=0; i<3; i++)
 			{
 				for(int j=0; j<5; j++)
+				{
+					if(laser_view[i][j] == 1)
 					{
-						if(laser_view[i][j] == 1)
-						{
-							*out_x = float(3 - (i + 1));
-							*out_y = float(5 - (j + 1) -2);
-							*out_z = float(0);
-							++out_x; ++out_y; ++out_z;
-						}
+						*out_x = float(3 - (i + 1));
+						*out_y = float(5 - (j + 1) -2);
+						*out_z = float(0);
+						++out_x; ++out_y; ++out_z;
 					}
+				}
 			}
 			cloud_generated = true;
+//			ROS_INFO("PointCloud generated");
 		}
 
 		void publish_pointCloud()
@@ -106,9 +111,11 @@ class Get_map_sensor_data
 			ros::Rate rate(2);
 			while(!cloud_generated && ros::ok())
 			{
-			rate.sleep();
+				ROS_INFO("Waiting for point_cloud to be generated");
+				rate.sleep();
 			}
-			while(cloud_generated && ros::ok())
+			cloud_published = true;
+			while(ros::ok())
 			{
 				cloud_pub_.publish(pointCloud);
 				pub_rate.sleep();
@@ -116,12 +123,45 @@ class Get_map_sensor_data
 
 		}
 
-		void update_robotPose()
+		void update_robotPose(int (*goal)[3])
 		{
-			robot_pose[0] = 0; // x-coordinate
-			robot_pose[1] = 2; // y-coordinate
-			robot_pose[2] = 0; // rotation about z- axis (CCW is positive)
+			bool goal_reached = false;
+			ros::Rate rate(robot_move_rate);
+			int pose_diff[3];
+//			ROS_INFO("Robot Pose before update x:%d, y:%d, theta:%d",robot_pose[0],robot_pose[1],robot_pose[2]);
+			while(!goal_reached && ros::ok)
+			{
+				robot_ready = false;
+				pose_diff[0] = (*goal)[0]-robot_pose[0]; pose_diff[1] = (*goal)[1]-robot_pose[1]; pose_diff[2] = (*goal)[2]-robot_pose[2];
+				if(pose_diff[0] != 0)
+				{
+					if(pose_diff[0] < 0)
+					{--robot_pose[0];}
+					else
+					{++robot_pose[0];}
+				}
+				else if(pose_diff[1] != 0)
+				{
+					if(pose_diff[1] < 0)
+					{--robot_pose[1];}
+					else
+					{++robot_pose[1];}
+				}
+				else if(pose_diff[2] != 0)
+				{
+					if(pose_diff[2] < 0)
+					{robot_pose[2] = robot_pose[2] - 90;}
+					else
+					{robot_pose[2] = robot_pose[2] + 90;}
+				}
+				else
+				{ goal_reached = true;}
+				Get_map_sensor_data::generate_pointCloud();
+//				ROS_INFO("Robot Pose x:%d, y:%d, theta:%d",robot_pose[0],robot_pose[1],robot_pose[2]);
 
+				rate.sleep();
+			}
+			robot_ready = true;
 		}
 
 		void get_laser_view(int idx,int robot_pose[3], int (*laser_view)[3][5], int *num_visible_obst)
@@ -196,11 +236,34 @@ class Get_map_sensor_data
 			for (int i=0; i<3; i++)
 			{
 				for(int j=0; j<5; j++)
-					{
-						if((*laser_view)[i][j] == 1){*num_visible_obst = *num_visible_obst + 1;}
-					}
+				{
+					if((*laser_view)[i][j] == 1){*num_visible_obst = *num_visible_obst + 1;}
+				}
 			}
 
+		}
+
+		void move_robot()
+		{
+			int goal[3];
+			int way_point_size = (sizeof(way_points))/(sizeof(way_points[0]));
+			ros::Rate rate(1);
+			while(!cloud_published)
+			{
+				ROS_INFO("Waiting for cloud to be published");
+				rate.sleep();
+			}
+			ROS_INFO("started move robot");
+			for(int g=0; g<way_point_size; g++)
+			{
+				goal[0] = way_points[g][0]; goal[1] = way_points[g][1]; goal[2] = way_points[g][2];
+				while(!robot_ready && ros::ok)
+				{ 
+					ROS_INFO("Waiting for robot to receive goal");
+					rate.sleep();
+				}
+				update_robotPose(&goal);
+			}
 		}
 };
 
@@ -209,9 +272,14 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "sensor_data");
 
   Get_map_sensor_data get_map;
-  ros::NodeHandle nh_;
 
-  ros::spin();
+  boost::thread* thread1 = new boost::thread(boost::bind(&Get_map_sensor_data::publish_pointCloud,&get_map));
+  boost::thread* thread2 = new boost::thread(boost::bind(&Get_map_sensor_data::move_robot,&get_map));
+
+  thread2->join();
+  thread1->join();
+
+  //ros::spin();
 
   return 0;
 }
